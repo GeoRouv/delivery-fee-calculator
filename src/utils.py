5b176d5e96fe0b2bcd85f493a19ab7a80ab871ad
George Rouvalis <giorgosrouvv@gmail.com>
2022-01-25 23:18:29 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,10 @@
 from datetime import datetime, time
 
 def is_time_between(begin_time, end_time, check_time=None):
+    '''
+    Checks if given datetime is between a range of two other datetimes
+    '''
+    
     # If check time is not given, default to current UTC time
     check_time = check_time or datetime.utcnow().time()
 
